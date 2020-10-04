997405987fc830a491206ccdc1d588cbc3bd2e85
Dan Picton <daniel.picton@gmail.com>
2020-10-04 17:37:55 +0100

    Rename PUBLISH_ROOT to PUBLISH_SOURCE_ROOT

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ import frontmatter
 import markdownpage
 
 # properties
-PUBLISH_ROOT = "/home/dan/Documents/pkm/ever-nearly-ready"
+PUBLISH_SOURCE_ROOT = "/home/dan/Documents/pkm/ever-nearly-ready"
 FILES_TO_TEST_WITH = ["Landscaping my website.md", "Digital Gardens.md"]
 HUGO_GIT_PATH = "/home/dan/projects/temp_python/ape-in-progress"
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    mdb = markdownbase.MarkdownBase(PUBLISH_ROOT)
+    mdb = markdownbase.MarkdownBase(PUBLISH_SOURCE_ROOT)
     mdb.index_files(FILES_TO_TEST_WITH)  # this will feed build_page_models
-    mdb.build_page_models(PUBLISH_ROOT, FILES_TO_TEST_WITH)
+    mdb.build_page_models(PUBLISH_SOURCE_ROOT, FILES_TO_TEST_WITH)
     mdb.build_publish_list()
 
     #output exceptions
