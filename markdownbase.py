d5a446f3efd15f8543f243ec94d6f1d998cdf91d
Dan Picton <daniel.picton@gmail.com>
2020-10-04 13:32:36 +0100

    Create markdown base dir class

---
+++ markdownbase.py
@@ -0,0 +1,18 @@
+class MarkdownBase:
+    """
+    A class representing a markdown note folder
+    """
+
+    def __init__(self, directory):
+        self.directory = directory
+        self.md_files = []
+        self.dead_local_refs = {}
+        # self.dead_web_refs = {}
+
+    def page_index(dir_to_index: str):
+        for dir_to_walk in dirs_to_index:
+            for dirName, subdirList, fileList in os.walk(dir_to_walk):
+
+                print('Found directory: %s' % dirName)
+                for fname in fileList:
+                    print('\t%s' % fname)
