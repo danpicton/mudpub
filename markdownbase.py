ff69115579215c63ce2b625efd43a2c7376d3d16
Dan Picton <daniel.picton@gmail.com>
2020-10-04 14:02:39 +0100

    Modify index_files method to identify relative paths

--- markdownbase.py
+++ markdownbase.py
@@ -15,6 +15,20 @@ class MarkdownBase:
     def index_files(self, specific_pages: [str]):
         file_set = set()
 
-            print('Found directory: %s' % dirName)
-            for fname in fileList:
-                print('\t%s' % fname)
+        # Understand what this list comprehension is doing.
+        # https://stackoverflow.com/questions/1192978/python-get-relative-path-of-all-files-and-subfolders-in-a-directory
+        files = [os.path.relpath(os.path.join(dirpath, file), self.directory) for (dirpath, dirnames, filenames) in
+                 os.walk(self.directory) for file in filenames]
+]
+        for file in files:
+            print(file)
+
+        # for dirName, subdirList, fileList in os.walk(self.directory):
+        #
+        #     print('Found directory: %s' % dirName)
+        #     for fname in fileList:
+        #         rel_dir = os.path.relpath(dirName, self.directory)
+        #         rel_file = os.path.join(rel_dir, fname)
+        #         file_set.add(rel_file)
+        #         print('\t%s\t%s' % (dirName, fname))
+        #         print(rel_file)
