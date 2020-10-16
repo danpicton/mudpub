a09836293fcc61f4c76690ea6c569d699f4fb4a4
Dan Picton <daniel.picton@gmail.com>
2020-10-16 20:04:58 +0100

    Refactor to use command line args

--- main.py
+++ main.py
@@ -7,23 +7,33 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser(description='Publish a directory of markdown files.')
-    parser.add_argument('source', type=str, nargs=1, help='source markdown directory')
-    parser.add_argument('publish', type=str, nargs=1, help='target publish directory')
+    parser.add_argument('source', type=str, help='source markdown directory')
+    parser.add_argument('publish', type=str, help='target publish directory')
     parser.add_argument('--attachments', type=str, nargs=1, help='source attachments directory')
     parser.add_argument('--files', type=str, nargs='+', help='specific files to publish')
+    parser.add_argument('--deadurl', type=str, nargs=1, help='URL for deactivated link note')
 
     args = parser.parse_args()
 
-    mdb = markdownbase.MarkdownBase(PUBLISH_SOURCE_ROOT)
-    mdb.index_source(specific_pages=FILES_TO_TEST_WITH)  # this will feed build_page_models
-    mdb.index_source(ATTACHMENTS_DIRECTORY) # this can be called for specific files, later, according to links
+    if not args.attachments:
+        args.attachments = "attachments"
+
+    if not args.files:
+        args.files = []
+
+    if not args.deadurl:
+        args.deadurl = "https://github.com/danpicton/mudpub/blob/main/README.md#selective-publication"
+
+    mdb = markdownbase.MarkdownBase(args.source)
+    mdb.index_source(specific_pages=args.files)  # this will feed build_page_models
+    mdb.index_source(args.attachments)  # this can be called for specific files, later, according to links
     mdb.build_page_models()
     mdb.define_publish_list()
     # mdb.convert_local_refs()
-    mdb.sanitise_dead_links(DEAD_LINK_NOTE)
-    mdb.publish_markdown(PUBLISH_DIRECTORY)     # <- by this method too
+    mdb.sanitise_dead_links(args.deadurl)
+    mdb.publish_markdown(args.publish)  # <- by this method too
     mdb.publish_attachments()
-    #output exceptions
+    # output exceptions
 
 
 if __name__ == '__main__':
